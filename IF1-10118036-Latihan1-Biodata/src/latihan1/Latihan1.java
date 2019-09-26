c00bb5a57e31bbc1880bb57d8d2eac1e94bbb47f
Adrian C. Senna <55816324+Raykitsuu@users.noreply.github.com>
2019-09-26 19:26:38 +0700

    Add files via upload

---
+++ IF1-10118036-Latihan1-Biodata/src/latihan1/Latihan1.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan1;
+
+/**
+ *
+ * @author
+ * NAMA			: Adrian C. Senna
+ * KELAS		: IF-1
+ * NIM			: 10118036
+ * Deskripsi Program	: Program ini berisi program untuk menampilkan biodata author.
+ */
+public class Latihan1 {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+		System.out.println("BIODATA" + "\n" + "-------------------");
+		System.out.println("NAMA\t\t: Adrian Carlo Senna");
+		System.out.println("ALAMAT\t\t: Jalan Bunga Sepatu no. 14");   
+		System.out.println("TEMPAT LAHIR\t: Jakarta");
+		System.out.println("TANGGALLAHIR\t: 09 Juli 2000"); 
+		System.out.println("------------------");
+		System.out.println("HOBI\t\t: Rocket League, Drawing");
+		System.out.println("MAKES\t\t: Pizza");
+		System.out.println("MIKES\t\t: Wotah, Pyuu Wotah");
+	}
+	
+}
